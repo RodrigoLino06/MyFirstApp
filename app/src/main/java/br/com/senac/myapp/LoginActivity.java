1f693ce5a5c969a26e05957dec2840cc470b2f73
RodrigoLino06 <digo-lino@live.com>
2022-06-07 17:18:19 -0300

    mais commit

--- app/src/main/java/br/com/senac/myapp/LoginActivity.java
+++ app/src/main/java/br/com/senac/myapp/LoginActivity.java
@@ -3,12 +3,62 @@ package br.com.senac.myapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
+
+    //declarar as variáveis globais
+    EditText edtEmail, edtSenha;
+    Button btnLogin;
+    TextView txtRecuperarSenha
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_layout);
+
+        //apresentadno  as variáveis xml no java
+        edtEmail = findViewById(R.id.edtEmail);
+        edtSenha = findViewById(R.id.edtSenha);
+        btnLogin = findViewById(R.id.btnLogin);
+        txtRecuperarSenha = findViewById(R.id.txtRecuperaSenha);
+
+        //criando clique do botao
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Pegar os valores de e-mail  e senha do usuário
+
+                String email, senha;
+
+                email = edtEmail.getText().toString();
+                senha = edtSenha.getText().toString();
+
+                //criando a estrutura de decisão para acesso ao sistema
+                if (true){
+                    //entrar aqui...
+                }
+
+            }
+        });
+
+        txtRecuperarSenha.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+    }
+
+    public void abrirFacebook(View view) {
+        Toast.makeText(getApplicationContext(),
+                "Cliquei no botão do Facebook",
+                Toast.LENGTH_SHORT).show();
     }
 }
